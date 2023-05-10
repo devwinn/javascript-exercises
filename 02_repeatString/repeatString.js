b24a21386f52106541e2f8733bb482bb5848ed65
devwinn <devinwingo@gmail.com>
2023-05-09 23:43:35 -0700

    completed first 2 exercises
    all tests passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, numRepeats) {
+    let repeated = "";
+    if (numRepeats >= 0) {
+        for (let i = 0; i < numRepeats; i++) {
+            repeated += str
+        }
+        return repeated;
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
