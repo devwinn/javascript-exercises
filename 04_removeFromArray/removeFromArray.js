884ceb942d8de1ddc9689eb999bfa7c04918a5b3
devwinn <>
2023-05-23 21:43:29 -0700

    working solutions for TOP Fundamentals Pt 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    finalArray = [];
 
+    function removeArgs (arg) {
+        if (!args.includes(arg)) {
+            finalArray.push(arg);
+        }
+    };
+
+    array.forEach((item) => removeArgs(item));
+    return finalArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  celsius = (fTemp - 32) * 5 / 9;
+  return (Math.round(celsius * 10)) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  fahrenheit = (cTemp * 9 / 5) + 32;
+  return (Math.round(fahrenheit * 10)) / 10;
 };
 
 // Do not edit below this line
